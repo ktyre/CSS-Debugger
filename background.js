20a0566443866f4c7af4ffe28af75cc3f3cfa144
Phil Elauria <phelauri@microsoft.com>
2016-03-17 21:29:39 -0700

    CSS debug worky now

--- background.js
+++ background.js
@@ -1,13 +1,5 @@
-chrome.runtime.onMessage.addListener(function(res, sender, sendRes) {
+chrome.runtime.onConnect.addListener(function() {
   chrome.tabs.executeScript(null, {
-    // code: "debug()";
     file: "debugFunc.js"
   });
 });
-
-// function debug() {
-//   var all = document.getElementsByTagName("*");
-//   for (var i=0, max=all.length; i < max; i++) {
-//     all[i].style.border = "1px solid blue";
-//   };
-// };
--- debugButton.js
+++ debugButton.js
@@ -2,13 +2,6 @@ window.onload = function() {
   var el = document.getElementById('debug');
 
   el.addEventListener('click', function() {
-    chrome.runtime.getBackgroundPage(debug);
-  }, false);
+    chrome.runtime.connect();
+  }, true);
 }
-
-function debug() {
-  var all = document.getElementsByTagName("*");
-  for (var i=0, max=all.length; i < max; i++) {
-    all[i].style.border = "1px solid blue";
-  };
-};
